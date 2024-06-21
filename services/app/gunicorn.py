da80779c65093a395422cb43169fe4f58691ce3b
ja-green <16142871+ja-green@users.noreply.github.com>
2024-06-21 17:14:31 +0100

    feat(gunicorn.py): add logging configuration to gunicorn setup
    Enable detailed logging in gunicorn by adding access log and error log
    configurations. This includes setting the logging level to "info" and capturing
    output to ensure better monitoring and troubleshooting capabilities.

--- services/app/gunicorn.py
+++ services/app/gunicorn.py
@@ -9,6 +9,11 @@ max_requests_jitter = 50
 bind = "0.0.0.0:8000"
 workers = multiprocessing.cpu_count() * 2 + 1
 
+accesslog = "-"
+errorlog = "-"
+loglevel = "info"
+capture_output = True
+
 keyfile = "/etc/ssl/internal/app-key.pem"
 certfile = "/etc/ssl/internal/app-cert.pem"
 ca_certs = "/etc/ssl/internal/ca.pem"
