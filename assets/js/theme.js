63d10f63467525652801d622b2dc8cbc7a16eb19
ja-green <16142871+ja-green@users.noreply.github.com>
2024-07-14 22:17:34 +0100

    refactor(theme.js): remove image source update logic from dark mode toggle
    Simplify the dark mode toggle functionality in theme.js by removing the
    unneeded `updateImageSources` function and its associated calls.

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -6,25 +6,11 @@ document.addEventListener("DOMContentLoaded", function() {
         document.cookie = `theme=${theme}; path=/; expires=${expiration}; domain=jackgreen.co; SameSite=Lax`;
     }
 
-    function updateImageSources(isDarkMode) {
-        const darkThemeSources = document.querySelectorAll("source[data-theme=\"dark\"]");
-        const lightThemeSources = document.querySelectorAll("source[data-theme=\"light\"]");
-
-        if (isDarkMode) {
-            darkThemeSources.forEach(source => source.media = "(min-width: 0px)");
-            lightThemeSources.forEach(source => source.media = "not all");
-        } else {
-            darkThemeSources.forEach(source => source.media = "not all");
-            lightThemeSources.forEach(source => source.media = "(min-width: 0px)");
-        }
-    }
-
     if (darkModeButton) {
         darkModeButton.classList.remove("hidden");
 
         darkModeButton.addEventListener("click", function() {
             const isDarkMode = document.documentElement.classList.toggle("dark");
-            updateImageSources(isDarkMode);
             setThemeCookie(isDarkMode ? "dark" : "light");
         });
     }
